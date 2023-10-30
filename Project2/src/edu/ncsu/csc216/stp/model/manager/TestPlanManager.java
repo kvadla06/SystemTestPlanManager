e7f1af262016e8b0e8f30ac1307f1e69a129be8d
Kaushya Bhattu <krbhattu@ncsu.edu>
2023-10-30 19:25:57 -0400

    [Implementation]
    Created subpackages of the model package and the view package

---
+++ Project2/src/edu/ncsu/csc216/stp/model/manager/TestPlanManager.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.stp.model.manager;
+
+/**
+ * @author Kaush
+ *
+ */
+public class TestPlanManager {
+
+}
---
+++ Project2/src/edu/ncsu/csc216/stp/model/test_plans/TestPlan.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.stp.model.test_plans;
+
+/**
+ * @author Kaush
+ *
+ */
+public class TestPlan extends AbstractTestPlan{
+
+}
