2688cd3565e57811953328d8502a85f399a644f5
Kaushya Bhattu <krbhattu@ncsu.edu>
2023-11-17 03:36:48 -0500

    jenkins error

--- Project2/src/edu/ncsu/csc216/stp/model/tests/TestCase.java
+++ Project2/src/edu/ncsu/csc216/stp/model/tests/TestCase.java
@@ -82,7 +82,7 @@ public class TestCase {
 	 */
 	private void setTestType(String testType) {
 		if (testType == null || testType.trim().isEmpty()) {
-            throw new IllegalArgumentException("Invalid test information.");
+            throw new IllegalArgumentException("Invalid test results.");
         }
         this.testType = testType;
 	}
@@ -127,7 +127,7 @@ public class TestCase {
 	 */
 	private void setExpectedResults(String expectedResults) {
 		if (expectedResults == null || expectedResults.trim().isEmpty()) {
-            throw new IllegalArgumentException("Invalid test information.");
+            throw new IllegalArgumentException("Invalid test results.");
         }
         this.expectedResults = expectedResults;
 	}
@@ -144,7 +144,7 @@ public class TestCase {
             TestResult result = new TestResult(passing, actualResults);
             testResults.add(result);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Invalid test information.");
+            throw new IllegalArgumentException("Invalid test results.");
         }
 	}
 	
